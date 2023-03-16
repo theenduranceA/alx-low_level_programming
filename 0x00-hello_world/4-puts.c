2057953ad4554b466206fffc421f860f1e417eb1
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-16 14:23:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point that puts my first C program here
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
