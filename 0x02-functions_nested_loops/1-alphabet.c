db590a53e5e95b942cf262cfab1538c92272fb49
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-21 08:25:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - A function that prints the alphabet in lowercase
+ * Return: void
+ */
+
+int main void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+
+	_putchar('\n');
+}
