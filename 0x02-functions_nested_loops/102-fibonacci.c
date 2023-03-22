4ce3dcc0ee388fbb98068affe88fa0945c52f971
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-22 06:16:24 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,25 @@
 
 int main(void)
 {
-	int count;
+	int a = 0
 
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long b = 1, c = 2;
 
-	for (count = 0; count < 50; count++)
+	while (a < 50)
 	{
-	sum = fib1 + fib2;
-	printf("%lu", sum);
-
-	fib1 = fib2;
-	fib2 = sum;
-
-	if (count == 49)
-	printf("\n");
+	if (a == 0)
+	printf("%lu", b);
+	else if (a == 1)
+	printf(". %lu", c);
 	else
-	printf(",");
+	{
+	c += b;
+	b = c - b;
+	printf(". %lu", c);
 	}
+	++a;
+	}
+	printf("\n");
 	return (0);
 }
 
