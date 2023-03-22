77fd0394365fb60c6347e91cc6ee8fac5f4983d2
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-21 21:53:51 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,40 +12,23 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-	for (i = 0; i <= n; i++)
-	{
-	for (j = 0; j <= n; j++)
-	{
-	k = j * i;
-	if (j == 0)
-	{
-	_putchar(k + '0');
-	}
-	else if (k < 10 && j != 0)
+	for (i = 0; i <= n; j++)
 	{
 	_putchar(',');
 	_putchar(' ');
+	(k = i * j)
+	if (k <= 99)
 	_putchar(' ');
+	if (k <= 9)
 	_putchar(' ');
-	_putchar(k + '0');
-	}
-	else if (k >= 10 && k < 100)
+	if (k >= 100)
 	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
+	_putchar((k / 100) + '0');
+	_putchar((k / 10) % 10 + '0');
+	}
+	else if ((k <= 99 && k >= 10);
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
-	}
-	else if (k >= 100)	
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar((k / 100) + '0');
-	_putchar(((k / 10) % 10) + '0');
-	_putchar((k % 10) + '0');
-	}
-	}
 	_putchar('\n');
 	}
 	}
