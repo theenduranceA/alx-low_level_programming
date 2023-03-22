bdd254b146aaecaab0ffb35673ce1fb8d32ace65
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-22 11:41:32 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - To test for positive or negative function
+* Return: 0
+*/
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
