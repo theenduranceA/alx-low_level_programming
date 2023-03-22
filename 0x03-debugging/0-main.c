3aaa7c03f300bc83cea329e7ea2e54893f3d83f5
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-22 12:11:33 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* main - To test for positive or negative function
+* main - Prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
+
 {
 
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
