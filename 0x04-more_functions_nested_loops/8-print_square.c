aa4aa3da5ff828227b19252e511380c05877e287
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-23 13:07:40 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Function that prints a square.
+ * @size: The size of tge square.
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
