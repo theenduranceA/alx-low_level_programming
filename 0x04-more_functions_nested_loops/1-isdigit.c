ca52e780a3d0a818ea2706472836cb280c2c8a95
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-23 07:59:35 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Prints a digit from 0 to 9
+ *@c : The character to check
+ *
+ * Return: 1 if digit or 0 if otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	return (0);
+}
