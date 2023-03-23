c2fe8a7446a3f9874a5a89b55f1908c230f14900
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-23 09:02:14 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9.
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
