838aa483990dd46a76bfde0951968be893ea3f7c
Endurance Aneke <theenduranceaneke@gmail.com>
2023-03-29 19:23:13 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer
+ * @n: The integer to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	a = -n;
+	}
+
+	if (a / 10 != 0)
+	{
+	print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
+
