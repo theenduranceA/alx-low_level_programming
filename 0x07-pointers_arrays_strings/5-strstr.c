9f37d8fc251460b43bc0e599103d1e661cc20176
Endurance Aneke <theenduranceaneke@gmail.com>
2023-04-04 16:20:30 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - Function that locates a substring
+ * @haystack: The string to be searched
+ * @needle: The substring
+ * Return: A pointer or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *str1;
+	char *str2;
+
+	str1 = haystack;
+	str2 = needle;
+	for (; *haystack >= '\0'; haystack++)
+	{
+	while (*str1 == *str2 && *str2 >= '\0')
+	{
+		str1++;
+		str2++;
+	}
+	if (*str2 == '\0')
+	return (haystack);
+	}
+	return (0);
+}
