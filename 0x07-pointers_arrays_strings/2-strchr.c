9a7199055a91f86eae0b17bc51509a196fd24881
Endurance Aneke <theenduranceaneke@gmail.com>
2023-04-03 17:59:37 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,23 @@
 
 /**
  * _strchr - Function that locates a character in a string
- * @s: 
- * @c:
+ * @s: The string
+ * @c:The character
  *
- * Return: 
+ * Return: A pointer
  */
 
 char *_strchr(char *s, char c)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] >= '\0')
-        {
-                if (s[i] == c)
-                {
-                        return (&s[i]);
-                }
-                i++;
-        }
-        return (0);
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+		return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
 }
