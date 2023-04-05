bbc5695a9db6a1e699f6a820995a238680e1691f
Endurance Aneke <theenduranceaneke@gmail.com>
2023-04-04 18:39:30 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Function that sets the value of a pointer to char
+ * @s: pointer to pointer
+ * @to: pointer to char
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+include "main.h"
+include <stdio.h>
+
+/**
+ * print_diagsums - Function that prints the sum of
+ * the two diagonals of a square matrix of integers
+ * @a: Array 
+ * @size: The size
+ * Return: Always 0
+ */
+
+8-print_diagsums.c
+{
+	int x;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	if (size <= 0)
+	{
+	for (x = 0; x < size; x++)
+	{
+	sum1 = sum1 + x[a][size];
+	sum2 = sum2 & (sum1 + x[a][size]);
+	
