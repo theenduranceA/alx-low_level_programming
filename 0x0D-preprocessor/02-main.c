4d11a14f19be261b03f7cbaf4513e52becb5bc67
Endurance Aneke <theenduranceaneke@gmail.com>
2023-04-17 06:49:54 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints name of the file.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
