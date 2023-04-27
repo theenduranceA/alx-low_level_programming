a9ebcdd4b7360ce1f9ba33204510608439537024
theenduranceA <theenduranceaneke@gmail.com>
2023-04-27 09:22:12 -0700

    Task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+extern printf
+section .data
+	msg: db "Hello, Holberton",0
+	fmt: db "%s\n",0
+
+section .text
+	global main
+
+main:
+
+	push rbp
+	mov rbp, rsp
+
+	lea rdi, [fmt]
+	lea rsi, [msg]
+
+	mov eax, 0
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	mov eax, 0
+	ret
