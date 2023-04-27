62868eb6dafa37947d30ef9ba7ed01f8adc64854
theenduranceA <theenduranceaneke@gmail.com>
2023-04-27 06:17:08 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Function that prints all the elements of a list
+ * @h: The list to be printed
+ *
+ * Return: Number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	for (x = 0; h; x++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (x);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x12. C - Singly linked lists. C Algorithm Data Structure
