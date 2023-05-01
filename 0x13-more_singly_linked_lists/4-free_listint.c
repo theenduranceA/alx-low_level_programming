409946579ab139ea717f9a2eefcf7627c1923b12
theenduranceA <theenduranceaneke@gmail.com>
2023-05-01 09:32:54 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+  * free_listint_t - Function that frees a linked list
+  * @head: The beginning of the list
+  */
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
