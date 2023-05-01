a89b7ff05b394ff7a8b97f8a669eb99a4f2997dd
theenduranceA <theenduranceaneke@gmail.com>
2023-05-01 10:33:15 -0700

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * pop_listint - pops off the head of the list and returns its contents
+  * @head: head of the list
+  * Return: contents of head
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *zed;
+	int x;
+
+	if (head == NULL)
+		return (0);
+	zed = *head;
+	if (*head)
+	{
+		x = zed->n;
+		*head = zed->next;
+		free(zed);
+	}
+	else 
+		x = 0;
+	return (x);
+}
