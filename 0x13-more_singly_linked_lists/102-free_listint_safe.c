fc61512380d23c99e92b2ca7732e8d8a44c01383
theenduranceA <theenduranceaneke@gmail.com>
2023-05-02 15:21:12 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,31 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-  * free_listint_safe - define function
-  * @h:
-  * Return: what does it return?
+  * free_listint_safe - Function that frees a list
+  * @h: Double pointer to the list
+  *
+  * Return: Size of the list that was freed
   */
+
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	size_t x = 0;
+	listint_t *zed;
+
+	while (*h && *h > (*h)->next)
+	{
+		zed = (*h)->next;
+		free(*h);
+		*h = zed;
+		x++;
+	}
+	if (*h)
+	{
+		free(*h);
+		x++;
+	}
+	*h = NULL;
+	return (x);
 }
