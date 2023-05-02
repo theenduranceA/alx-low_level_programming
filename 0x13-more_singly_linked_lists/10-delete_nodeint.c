7535dba5b40d10269f9cdef810790a8a21c2f1c6
theenduranceA <theenduranceaneke@gmail.com>
2023-05-02 05:00:26 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (current == NULL)
 		return (-1);
 	if (current->next == NULL)
-		return(-1);
+		return (-1);
 
 	next = current->next->next;
 	free(current->next);
